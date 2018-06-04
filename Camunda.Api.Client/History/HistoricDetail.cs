3b221733083896fbc9a7ee7f32b6512a194c5775
Michal.Jablonski <Michal.Jablonski@partnersinprogress.eu>
2018-06-04 11:20:23 +0200

    historic detail
    IHistoricDetailRestService 
    Merge remote-tracking branch 'origin/master'
    
    
    Add resource historic detail to client

---
+++ Camunda.Api.Client/History/HistoricDetail.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Camunda.Api.Client.History
+{
+    public class HistoricDetail
+    {
+        /// <summary>
+        /// Type
+        /// </summary>
+        public string Type;
+        /// <summary>
+        /// The id of the entry.
+        /// </summary>
+        public string Id;
+        /// <summary>
+        /// The key of the process definition.
+        /// </summary>
+        public string ProcessDefinitionKey;
+        /// <summary>
+        /// The id of the process definition which the associated job belongs to.
+        /// </summary>
+        public string ProcessDefinitionId;
+        /// <summary>
+        /// The id of the process instance on which the associated job was created.
+        /// </summary>
+        public string ProcessInstanceId;
+        /// <summary>
+        /// The id of the activity instance that the external task belongs to.
+        /// </summary>
+        public string ActivityInstanceId;
+        /// <summary>
+        /// The execution id on which the associated job was created.
+        /// </summary>
+        public string ExecutionId;
+        /// <summary>
+        /// Case definition key
+        /// </summary>
+        public string CaseDefinitionKey;
+        /// <summary>
+        /// Case definition id
+        /// </summary>
+        public string CaseDefinitionId;
+        /// <summary>
+        /// Case instance id
+        /// </summary>
+       public string CaseInstanceId;
+        /// <summary>
+        /// Case execution id
+        /// </summary>
+        public string CaseExecutionId;
+        /// <summary>
+        /// Task id
+        /// </summary>
+        public string TaskId;
+        /// <summary>
+        /// The id of the tenant that this historic job log entry belongs to.
+        /// </summary>
+        public string TenantId;
+        /// <summary>
+        /// User operation id
+        /// </summary>
+        public string UserOperationId;
+        /// <summary>
+        /// Time
+        /// </summary>
+        public string Time;
+        /// <summary>
+        /// Variable name
+        /// </summary>
+        public string VariableName;
+        /// <summary>
+        /// Variable instance id
+        /// </summary>
+        public string VariableInstanceId;
+        /// <summary>
+        /// Variable type
+        /// </summary>
+        public string VariableType;
+        /// <summary>
+        /// Value
+        /// </summary>
+        public object Value;
+        /// <summary>
+        /// Revision
+        /// </summary>
+        public string revision;
+        /// <summary>
+        /// Error message
+        /// </summary>
+          public string errorMessage;
+        /// <summary>
+        /// Value info
+        /// </summary>
+        public Dictionary<string, object> ValueInfo; 
+        /// <summary>
+        /// To string implementation
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() => Id.ToString();
+    }
+}
---
+++ Camunda.Api.Client/History/HistoricDetailService.cs
@@ -0,0 +1,18 @@
+﻿namespace Camunda.Api.Client.History
+{
+    public class HistoricDetailService
+    {
+        private IHistoricDetailRestService _api;
+
+        internal HistoricDetailService(IHistoricDetailRestService api)
+        {
+            _api = api;
+        }
+
+        public QueryResource<HistoricDetailQuery, HistoricDetail> Query(HistoricDetailQuery query = null) =>
+            new QueryResource<HistoricDetailQuery, HistoricDetail>(_api, query);
+
+        /// <param name="historicJobLogId">The id of the detail entry.</param>
+        public HistoricDetailResource this[string historicJobLogId] => new HistoricDetailResource(_api, historicJobLogId);
+    }
+}
