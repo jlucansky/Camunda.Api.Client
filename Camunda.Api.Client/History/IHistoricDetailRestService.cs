8f78f0750c3edb602780c291ec50a71f72dd82c0
Michal.Jablonski <Michal.Jablonski@partnersinprogress.eu>
2018-06-04 11:06:56 +0200

    IHistoricDetailRestService

---
+++ Camunda.Api.Client/History/IHistoricDetailRestService.cs
@@ -0,0 +1,22 @@
+﻿using Refit;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Camunda.Api.Client.History
+{
+    internal interface IHistoricDetailRestService
+    {
+        [Get("/history/detail/{id}")]
+        Task<HistoricDetail> Get(string id);
+
+        [Get("/history/detail")]
+        Task<List<HistoricDetail>> GetList(QueryDictionary query, int? firstResult, int? maxResults);
+
+        [Get("/history/detail/count")]
+        Task<CountResult> GetListCount(QueryDictionary query);
+
+        [Get("/history/detail/{id}/data")]
+        Task<HttpResponseMessage> GetData(string id);
+    }
+}
