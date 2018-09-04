14b0ad9c1f80dae7fb64433b8086a1b7e19c897b
Marco van den Oever <marco@flyingpie.nl>
2018-09-04 08:55:12 +0200

    SignalService

--- Camunda.Api.Client/CamundaClient.cs
+++ Camunda.Api.Client/CamundaClient.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Net.Http;
 using System.Reflection;
-using System.Globalization;
 
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
@@ -13,20 +12,19 @@ using Camunda.Api.Client.DecisionDefinition;
 using Camunda.Api.Client.Deployment;
 using Camunda.Api.Client.Execution;
 using Camunda.Api.Client.ExternalTask;
+using Camunda.Api.Client.Group;
+using Camunda.Api.Client.History;
+using Camunda.Api.Client.Incident;
+using Camunda.Api.Client.Job;
+using Camunda.Api.Client.JobDefinition;
+using Camunda.Api.Client.Message;
 using Camunda.Api.Client.ProcessDefinition;
 using Camunda.Api.Client.ProcessInstance;
+using Camunda.Api.Client.Signal;
+using Camunda.Api.Client.Tenant;
+using Camunda.Api.Client.User;
 using Camunda.Api.Client.UserTask;
 using Camunda.Api.Client.VariableInstance;
-using Camunda.Api.Client.Message;
-using Camunda.Api.Client.JobDefinition;
-using Camunda.Api.Client.Job;
-using Camunda.Api.Client.Incident;
-using Camunda.Api.Client.History;
-using Camunda.Api.Client.User;
-using Camunda.Api.Client.Tenant;
-
-using System.Net.Http;
-using System.Reflection;
 
 namespace Camunda.Api.Client
 {
@@ -35,56 +33,55 @@ namespace Camunda.Api.Client
         private Lazy<ICaseDefinitionRestService> _caseDefinitionRestService;
         private Lazy<ICaseExecutionRestService> _caseExecutionRestService;
         private Lazy<IDecisionDefinitionRestService> _decisionDefinitionRestService;
-        private Lazy<IExternalTaskRestService> _externalTaskApi;
-        private Lazy<IProcessInstanceRestService> _processInstanceApi;
-        private Lazy<IVariableInstanceRestService> _variableInstanceApi;
-        private Lazy<IProcessDefinitionRestService> _processDefinitionApi;
         private Lazy<IDeploymentRestService> _deploymentApi;
-        private Lazy<IUserTaskRestService> _userTaskApi;
         private Lazy<IExecutionRestService> _executionApi;
-        private Lazy<IMessageRestService> _messageApi;
+        private Lazy<IExternalTaskRestService> _externalTaskApi;
+        private Lazy<IGroupRestService> _groupApi;
+        private Lazy<IIncidentRestService> _incidentApi;
         private Lazy<IJobDefinitionRestService> _jobDefinitionApi;
         private Lazy<IJobRestService> _jobApi;
-        private Lazy<IIncidentRestService> _incidentApi;
+        private Lazy<IMessageRestService> _messageApi;
+        private Lazy<IProcessDefinitionRestService> _processDefinitionApi;
+        private Lazy<IProcessInstanceRestService> _processInstanceApi;
+        private Lazy<ISignalRestService> _signalApi;
         private Lazy<ITenantRestService> _tenantApi;
         private Lazy<IUserRestService> _userApi;
-		private Lazy<IGroupRestService> _groupApi;
+        private Lazy<IUserTaskRestService> _userTaskApi;
+        private Lazy<IVariableInstanceRestService> _variableInstanceApi;
 
-		private HistoricApi _historicApi;
+        private HistoricApi _historicApi;
 
         private string _hostUrl;
         private HttpClient _httpClient;
 
         private RefitSettings _refitSettings;
-        private static JsonSerializerSettings _jsonSerializerSettings;
         private HttpMessageHandler _httpMessageHandler;
 
-        internal static JsonSerializerSettings JsonSerializerSettings => _jsonSerializerSettings;
+        internal static JsonSerializerSettings JsonSerializerSettings { get; private set; }
 
         internal class HistoricApi
         {
+            public Lazy<IHistoricActivityInstanceRestService> ActivityInstanceApi;
+            public Lazy<IHistoricCaseActivityInstanceRestService> CaseActivityInstanceApi;
             public Lazy<IHistoricCaseDefinitionRestService> CaseDefinitionApi;
             public Lazy<IHistoricCaseInstanceRestService> CaseInstanceApi;
-            public Lazy<IHistoricCaseActivityInstanceRestService> CaseActivityInstanceApi;
             public Lazy<IHistoricDecisionInstanceRestService> DecisionInstanceApi;
-            public Lazy<IHistoricProcessInstanceRestService> ProcessInstanceApi;
-            public Lazy<IHistoricActivityInstanceRestService> ActivityInstanceApi;
-            public Lazy<IHistoricJobLogRestService> JobLogApi;
-            public Lazy<IHistoricIncidentRestService> IncidentApi;
-            public Lazy<IHistoricVariableInstanceRestService> VariableInstanceApi;
             public Lazy<IHistoricDetailRestService> DetailApi;
+            public Lazy<IHistoricIncidentRestService> IncidentApi;
+            public Lazy<IHistoricJobLogRestService> JobLogApi;
+            public Lazy<IHistoricProcessInstanceRestService> ProcessInstanceApi;
+            public Lazy<IHistoricVariableInstanceRestService> VariableInstanceApi;
         }
 
         static CamundaClient()
         {
-
-            _jsonSerializerSettings = _jsonSerializerSettings ?? new JsonSerializerSettings
+            JsonSerializerSettings = JsonSerializerSettings ?? new JsonSerializerSettings
             {
                 ContractResolver = new CustomCamelCasePropertyNamesContractResolver(),
                 NullValueHandling = NullValueHandling.Ignore, // not send empty fields
             };
 
-            _jsonSerializerSettings.Converters.Add(new StringEnumConverter());
+            JsonSerializerSettings.Converters.Add(new StringEnumConverter());
         }
 
         private void Initialize()
@@ -93,21 +90,10 @@ namespace Camunda.Api.Client
 
             _refitSettings = _refitSettings ?? new RefitSettings
             {
-                JsonSerializerSettings = _jsonSerializerSettings,
+                JsonSerializerSettings = JsonSerializerSettings,
                 HttpMessageHandlerFactory = () => _httpMessageHandler,
                 UrlParameterFormatter = new CustomUrlParameterFormatter(),
             };
-        }
-
-        private class CustomUrlParameterFormatter : DefaultUrlParameterFormatter
-        {
-            public override string Format(object parameterValue, ParameterInfo parameterInfo)
-            {
-                if (parameterValue is bool)
-                    return string.Format(CultureInfo.InvariantCulture, "{0}", parameterValue).ToLower();
-                else
-                    return base.Format(parameterValue, parameterInfo);
-            }
         }
 
         private class CustomCamelCasePropertyNamesContractResolver : CamelCasePropertyNamesContractResolver
@@ -154,33 +140,34 @@ namespace Camunda.Api.Client
             _caseDefinitionRestService = CreateService<ICaseDefinitionRestService>();
             _caseExecutionRestService = CreateService<ICaseExecutionRestService>();
             _decisionDefinitionRestService = CreateService<IDecisionDefinitionRestService>();
-            _userApi = CreateService<IUserRestService>();
-            _externalTaskApi = CreateService<IExternalTaskRestService>();
-            _processInstanceApi = CreateService<IProcessInstanceRestService>();
-            _variableInstanceApi = CreateService<IVariableInstanceRestService>();
-            _processDefinitionApi = CreateService<IProcessDefinitionRestService>();
             _deploymentApi = CreateService<IDeploymentRestService>();
-            _tenantApi = CreateService<ITenantRestService>();
-            _userTaskApi = CreateService<IUserTaskRestService>();
             _executionApi = CreateService<IExecutionRestService>();
-            _messageApi = CreateService<IMessageRestService>();
-            _jobDefinitionApi = CreateService<IJobDefinitionRestService>();
-            _jobApi = CreateService<IJobRestService>();
+            _externalTaskApi = CreateService<IExternalTaskRestService>();
+            _groupApi = CreateService<IGroupRestService>();
             _incidentApi = CreateService<IIncidentRestService>();
-			_groupApi = CreateService<IGroupRestService>();
+            _jobApi = CreateService<IJobRestService>();
+            _jobDefinitionApi = CreateService<IJobDefinitionRestService>();
+            _messageApi = CreateService<IMessageRestService>();
+            _processDefinitionApi = CreateService<IProcessDefinitionRestService>();
+            _processInstanceApi = CreateService<IProcessInstanceRestService>();
+            _signalApi = CreateService<ISignalRestService>();
+            _tenantApi = CreateService<ITenantRestService>();
+            _userApi = CreateService<IUserRestService>();
+            _userTaskApi = CreateService<IUserTaskRestService>();
+            _variableInstanceApi = CreateService<IVariableInstanceRestService>();
 
-			_historicApi = new HistoricApi()
+            _historicApi = new HistoricApi()
             {
+                ActivityInstanceApi = CreateService<IHistoricActivityInstanceRestService>(),
+                CaseActivityInstanceApi = CreateService<IHistoricCaseActivityInstanceRestService>(),
                 CaseDefinitionApi = CreateService<IHistoricCaseDefinitionRestService>(),
                 CaseInstanceApi = CreateService<IHistoricCaseInstanceRestService>(),
-                CaseActivityInstanceApi = CreateService<IHistoricCaseActivityInstanceRestService>(),
                 DecisionInstanceApi = CreateService<IHistoricDecisionInstanceRestService>(),
-                ProcessInstanceApi = CreateService<IHistoricProcessInstanceRestService>(),
-                ActivityInstanceApi = CreateService<IHistoricActivityInstanceRestService>(),
-                JobLogApi = CreateService<IHistoricJobLogRestService>(),
-                IncidentApi = CreateService<IHistoricIncidentRestService>(),
-                VariableInstanceApi = CreateService<IHistoricVariableInstanceRestService>(),
                 DetailApi = CreateService<IHistoricDetailRestService>(),
+                IncidentApi = CreateService<IHistoricIncidentRestService>(),
+                JobLogApi = CreateService<IHistoricJobLogRestService>(),
+                ProcessInstanceApi = CreateService<IHistoricProcessInstanceRestService>(),
+                VariableInstanceApi = CreateService<IHistoricVariableInstanceRestService>(),
             };
         }
 
@@ -207,59 +194,61 @@ namespace Camunda.Api.Client
             return new CamundaClient(httpClient);
         }
 
-
-        /// <see href="https://docs.camunda.org/manual/7.6/reference/rest/user/"/>
-        public UserService Users => new UserService(_userApi.Value);
-
-        /// <see href="https://docs.camunda.org/manual/7.6/reference/rest/case-definition/"/>
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/case-definition/"/>
         public CaseDefinitionService CaseDefinitions => new CaseDefinitionService(_caseDefinitionRestService.Value);
 
-        /// <see href="https://docs.camunda.org/manual/7.6/reference/rest/case-execution/"/>
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/case-execution/"/>
         public CaseExecutionService CaseExecutions => new CaseExecutionService(_caseExecutionRestService.Value);
 
-        /// <see href="https://docs.camunda.org/manual/7.6/reference/rest/decision-definition/"/>
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/decision-definition/"/>
         public DecisionDefinitionService DecisionDefinitions => new DecisionDefinitionService(_decisionDefinitionRestService.Value);
 
-        /// <see href="https://docs.camunda.org/manual/7.6/reference/rest/external-task/"/>
-        public ExternalTaskService ExternalTasks => new ExternalTaskService(_externalTaskApi.Value);
-
-        /// <see href="https://docs.camunda.org/manual/7.6/reference/rest/process-instance/"/>
-        public ProcessInstanceService ProcessInstances => new ProcessInstanceService(_processInstanceApi.Value);
-
-        /// <see href="https://docs.camunda.org/manual/7.6/reference/rest/variable-instance/"/>
-        public VariableInstanceService VariableInstances => new VariableInstanceService(_variableInstanceApi.Value);
-
-        /// <see href="https://docs.camunda.org/manual/7.6/reference/rest/process-definition/"/>
-        public ProcessDefinitionService ProcessDefinitions => new ProcessDefinitionService(_processDefinitionApi.Value);
-
-        /// <see href="https://docs.camunda.org/manual/7.6/reference/rest/deployment/"/>
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/deployment/"/>
         public DeploymentService Deployments => new DeploymentService(_deploymentApi.Value);
 
-        /// <see href="https://docs.camunda.org/manual/7.6/reference/rest/task/"/>
-        public UserTaskService UserTasks => new UserTaskService(_userTaskApi.Value);
-
-        /// <see href="https://docs.camunda.org/manual/7.6/reference/rest/execution/"/>
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/execution/"/>
         public ExecutionService Executions => new ExecutionService(_executionApi.Value);
 
-        /// <see href="https://docs.camunda.org/manual/7.6/reference/rest/message/"/>
-        public MessageService Messages => new MessageService(_messageApi.Value);
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/external-task/"/>
+        public ExternalTaskService ExternalTasks => new ExternalTaskService(_externalTaskApi.Value);
 
-        /// <see href="https://docs.camunda.org/manual/7.6/reference/rest/job-definition/"/>
-        public JobDefinitionService JobDefinitions => new JobDefinitionService(_jobDefinitionApi.Value);
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/group/"/>
+        public GroupService Group => new GroupService(_groupApi.Value);
 
-        /// <see href="https://docs.camunda.org/manual/7.6/reference/rest/job/"/>
-        public JobService Jobs => new JobService(_jobApi.Value);
-
-        /// <see href="https://docs.camunda.org/manual/7.6/reference/rest/incident/"/>
-        public IncidentService Incidents => new IncidentService(_incidentApi.Value);
-
-        /// <see href="https://docs.camunda.org/manual/7.6/reference/rest/history/"/>
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/history/"/>
         public HistoryService History => new HistoryService(_historicApi);
 
-		/// <see href="https://docs.camunda.org/manual/7.6/reference/rest/group/"/>
-		public GroupService Group => new GroupService(_groupApi.Value);
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/incident/"/>
+        public IncidentService Incidents => new IncidentService(_incidentApi.Value);
 
-        /// <see href="https://docs.camunda.org/manual/7.6/reference/rest/tenant/"/>
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/job-definition/"/>
+        public JobDefinitionService JobDefinitions => new JobDefinitionService(_jobDefinitionApi.Value);
+
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/job/"/>
+        public JobService Jobs => new JobService(_jobApi.Value);
+
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/message/"/>
+        public MessageService Messages => new MessageService(_messageApi.Value);
+
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/process-definition/"/>
+        public ProcessDefinitionService ProcessDefinitions => new ProcessDefinitionService(_processDefinitionApi.Value);
+
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/process-instance/"/>
+        public ProcessInstanceService ProcessInstances => new ProcessInstanceService(_processInstanceApi.Value);
+
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/signal/"/>
+        public SignalService Signals => new SignalService(_signalApi.Value);
+
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/task/"/>
         public TenantService Tenants => new TenantService(_tenantApi.Value);
+
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/tenant/"/>
+        public UserService Users => new UserService(_userApi.Value);
+
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/user/"/>
+        public UserTaskService UserTasks => new UserTaskService(_userTaskApi.Value);
+
+        /// <see href="https://docs.camunda.org/manual/7.9/reference/rest/variable-instance/"/>
+        public VariableInstanceService VariableInstances => new VariableInstanceService(_variableInstanceApi.Value);
     }
 }
---
+++ Camunda.Api.Client/Signal/Signal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Camunda.Api.Client.Signal
+{
+    public class Signal
+    {
+        /// <summary>
+        /// The name of the signal to deliver.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Optionally specifies a single execution which is notified by the signal.
+        /// </summary>
+        public string ExecutionId { get; set; }
+
+        /// <summary>
+        /// A JSON object containing variable key-value pairs. Each key is a variable name and each value a JSON variable value object.
+        /// </summary>
+        public Dictionary<string, VariableValue> Variables { get; set; }
+
+        /// <summary>
+        /// Specifies a tenant to deliver the signal. The signal can only be received on executions or process definitions which belongs to the given tenant.
+        /// </summary>
+        public string TenantId { get; set; }
+
+        /// <summary>
+        /// If true the signal can only be received on executions or process definitions which belongs to no tenant. Value may not be false as this is the default behavior.
+        /// </summary>
+        public bool WithoutTenantId { get; set; }
+    }
+}
