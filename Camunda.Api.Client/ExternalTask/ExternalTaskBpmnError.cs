da1a836a0455b0baef625252200f07663d8930ee
jlucansky <lucansky@gmail.com>
2019-02-22 16:29:53 +0100

    Support ErrorMessage and Variables in ExternalTaskBpmnError

--- Camunda.Api.Client/ExternalTask/ExternalTaskBpmnError.cs
+++ Camunda.Api.Client/ExternalTask/ExternalTaskBpmnError.cs
@@ -1,4 +1,6 @@
 
+using System.Collections.Generic;
+
 namespace Camunda.Api.Client.ExternalTask
 {
     public class ExternalTaskBpmnError
@@ -12,6 +14,17 @@ namespace Camunda.Api.Client.ExternalTask
         /// A error code that indicates the predefined error. Is used to identify the BPMN error handler.
         /// </summary>
         public string ErrorCode;
+
+        /// <summary>
+        /// An error message that describes the error.
+        /// </summary>
+        public string ErrorMessage;
+
+        /// <summary>
+        /// Object containing variable key-value pairs.
+        /// </summary>
+        public Dictionary<string, VariableValue> Variables = new Dictionary<string, VariableValue>();
+
 
         public override string ToString() => ErrorCode;
     }
