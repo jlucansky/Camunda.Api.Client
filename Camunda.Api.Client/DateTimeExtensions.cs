1f65929abcad0e6735efc86a8ad0e187d3e1d1ba
Darren Hull <Darren.Hull@ats-global.com>
2019-03-06 16:31:50 +0000

    Fix Java Datetime serialisation issues.

--- Camunda.Api.Client/DateTimeExtensions.cs
+++ Camunda.Api.Client/DateTimeExtensions.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Globalization;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("Camunda.Api.Client.Tests")]
 namespace Camunda.Api.Client
 {
     internal static class DateTimeExtensions
     {
-        const string _dateTimeFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss.FFF";
+        const string _dateTimeFormat = "yyyy-MM-ddTHH\\:mm\\:ss.fffzzz";
 
         public static string ToJavaISO8601(this DateTime dateTime)
         {
-            return dateTime.ToString(_dateTimeFormat, CultureInfo.InvariantCulture)
-                + dateTime.ToString("%K").Replace(":", "").Replace("Z", "UTC");
+            return ReplaceLastOccurrence(dateTime.ToString(_dateTimeFormat, CultureInfo.InvariantCulture), ":", "");
+        }
+
+        private static string ReplaceLastOccurrence(string Source, string Find, string Replace)
+        {
+            int place = Source.LastIndexOf(Find);
+
+            if (place == -1)
+                return Source;
+
+            return Source.Remove(place, Find.Length).Insert(place, Replace);
+
         }
     }
 }
