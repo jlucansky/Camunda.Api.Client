e26133445e6a0f549c34106a35fe582cd58a23c5
Dominic Weilmünster <Dominic.Weilmuenster@soptim.de>
2019-07-29 13:10:13 +0200

    -Added AsyncResponseTimeout to add support for Long Polling

--- Camunda.Api.Client/ExternalTask/FetchExternalTasks.cs
+++ Camunda.Api.Client/ExternalTask/FetchExternalTasks.cs
@@ -20,6 +20,11 @@ namespace Camunda.Api.Client.ExternalTask
         public bool UsePriority;
 
         /// <summary>
+        /// Response timeout for long polling
+        /// </summary>
+        public long AsyncResponseTimeout;
+
+        /// <summary>
         /// Array of topic objects for which external tasks should be fetched. The returned tasks may be arbitrarily distributed among these topics.
         /// </summary>
         public List<FetchExternalTaskTopic> Topics;
