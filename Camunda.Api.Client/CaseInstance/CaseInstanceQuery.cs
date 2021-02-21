c888e9a40f10bec2104d5bfb30ada4fded1ab156
Mohammad Sharifi <sharifi8472@gmail.com>
2021-02-21 14:49:37 +0330

    Add CaseInstance service

--- Camunda.Api.Client/CaseInstance/CaseInstanceQuery.cs
+++ Camunda.Api.Client/CaseInstance/CaseInstanceQuery.cs
@@ -9,55 +9,41 @@ namespace Camunda.Api.Client.CaseInstance
 {
     public class CaseInstanceQuery
     {
-        [JsonProperty("caseInstanceId")]
         public string CaseInstanceId;
 
-        [JsonProperty("businessKey")]
         public string BusinessKey;
 
-        [JsonProperty("caseDefinitionId")]
         public string CaseDefinitionId;
 
-        [JsonProperty("caseDefinitionKey")]
         public string CaseDefinitionKey;
 
-        [JsonProperty("deploymentId")]
         public string DeploymentId;
 
-        [JsonProperty("superProcessInstance")]
         public string SuperProcessInstance;
 
-        [JsonProperty("subProcessInstance")]
         public string SubProcessInstance;
 
-        [JsonProperty("superCaseInstance")]
         public string SuperCaseInstance;
 
-        [JsonProperty("subCaseInstance")]
         public string SubCaseInstance;
 
-        [JsonProperty("active")]
         public bool? Active;
 
-        [JsonProperty("completed")]
         public bool? Completed;
 
         [JsonProperty("tenantIdIn")]
-        public List<string> TenantIdIn;
+        public List<string> TenantIds;
 
-        [JsonProperty("withoutTenantId")]
         public bool? WithoutTenantId;
 
-        [JsonProperty("variables")]
-        public List<CaseInstanceQueryVariable> Variables;
+        public List<VariableQueryParameter> Variables;
 
-        [JsonProperty("variableNamesIgnoreCase")]
         public bool VariableNamesIgnoreCase;
 
-        [JsonProperty("variableValuesIgnoreCase")]
         public bool VariableValuesIgnoreCase;
 
-        [JsonProperty("sorting")]
-        public List<CaseInstanceSorting> Sorting;
+        public CaseInstanceSorting SortBy;
+
+        public SortOrder SortOrder;
     }
 }
