03dddb783e5f1d3ac0153ef79ade862996246c9a
Wilfried Ricken <wilfried.ricken@cbc.de>
2021-03-19 15:12:06 +0100

    Added Version interface

--- Camunda.Api.Client.Tests/ProcessInstanceTests.cs
+++ Camunda.Api.Client.Tests/ProcessInstanceTests.cs
@@ -25,5 +25,20 @@ namespace Camunda.Api.Client.Tests
 
             Assert.NotNull(process);
         }
+
+        [Fact]
+        public async Task GetVersion()
+        {
+            var mockHttp = new MockHttpMessageHandler();
+
+            mockHttp.Expect(HttpMethod.Get, "http://localhost:8080/engine-rest/version")
+                .Respond(HttpStatusCode.OK, "text/html", "{\"version\": \"7.14.0\"}");
+
+
+            var client = CamundaClient.Create("http://localhost:8080/engine-rest", mockHttp);
+            var version = await client.Version.Get();
+
+            Assert.Equal("7.14.0", version.Version);
+        }
     }
 }
