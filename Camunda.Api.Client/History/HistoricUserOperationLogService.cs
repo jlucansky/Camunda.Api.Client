052a802636ed0ff3e0f3f20cc91ac1cdf7a79ec0
avalente88 <62705781+avalente88@users.noreply.github.com>
2021-11-18 23:15:52 +0000

    deleted repeted classes

--- Camunda.Api.Client/History/HistoricUserOperationLogService.cs
+++ Camunda.Api.Client/History/HistoricUserOperationLogService.cs
@@ -17,12 +17,14 @@ namespace Camunda.Api.Client.History
         /// Get all the user assignments that were made for the processInstanceId
         /// It can also be done with the Query method.
         /// </summary>
+        /// <param name="processInstanceId">The id of the process instance.</param>
         public Task<List<HistoricUserOperationLog>> GetHistoricAssignmentsOfProcess(string processInstanceId) => _api.GetHistoricAssignmentsOfProcess(processInstanceId);
 
         /// <summary>
         /// Get all the user assignments that were made for the taskInstanceId
         /// It can also be done with the Query method.
         /// </summary>
+        /// <param name="taskInstanceId">The id of the task instance.</param>
         public Task<List<HistoricUserOperationLog>> GetHistoricAssignmentsOfTask(string taskInstanceId) => _api.GetHistoricAssignmentsOfTask(taskInstanceId);
 
         public QueryResource<HistoricUserOperationLogQuery, HistoricUserOperationLog> Query(HistoricUserOperationLogQuery query = null) =>
@@ -32,7 +34,7 @@ namespace Camunda.Api.Client.History
                  q => _api.GetListCount(q));
 
         /// <param name="historicOperationId">The id of the operation log entry.</param>
-         public HistoricUserOperationLogResource this[string historicOperationId] => new HistoricUserOperationLogResource(_api, historicOperationId);
+        public HistoricUserOperationLogResource this[string historicOperationId] => new HistoricUserOperationLogResource(_api, historicOperationId);
 
     }
 }
