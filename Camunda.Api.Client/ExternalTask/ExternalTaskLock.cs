13499dd67675878799c418dda0598eab99d87f65
Christian Kittel <ckittel@gmx.de>
2022-02-19 12:09:57 +0100

    externalTask/Lock added
    Signed-off-by: Christian Kittel <ckittel@gmx.de>

---
+++ Camunda.Api.Client/ExternalTask/ExternalTaskLock.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Camunda.Api.Client.ExternalTask
+{
+  public class ExternalTaskLock
+  {
+    /// <summary>
+    /// The ID of a worker who is locking the external task.
+    /// </summary>
+    public string WorkerId;
+
+    /// <summary>
+    /// An amount of time (in milliseconds) to extend the lock by.
+    /// </summary>
+    public long Duration;
+  }
+}
